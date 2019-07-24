fc20a3ebc319bde04ac71c0effbe4d4633769164
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-07-24 17:53:01 -0300

    Estilo para sign(in/up)

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="text" placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="text" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
