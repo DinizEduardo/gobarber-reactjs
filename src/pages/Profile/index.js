6c00212ac2f9ba82e26fd472a19818e4fabd2ac9
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-07-26 16:52:34 -0300

    Fazendo o logout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Container } from './styles';
 
 import { updateProfileRequest } from '~/store/modules/user/actions';
-
+import { signOut } from '~/store/modules/auth/actions';
 import AvatarInput from './AvatarInput';
 
 export default function Profile() {
@@ -12,6 +12,10 @@ export default function Profile() {
   const dispatch = useDispatch();
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
   }
 
   return (
@@ -38,7 +42,9 @@ export default function Profile() {
         <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button onClick={handleSignOut} type="button">
+        Sair do GoBarber
+      </button>
     </Container>
   );
 }
